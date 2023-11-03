ad3e4b62ea32e69da5cf51399f9d2ed658c37743
keroti <peterkeroti55@gmail.com>
2023-11-03 20:19:24 +0300

    'New'

--- shutterlinkProj/settings.py
+++ shutterlinkProj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$5x#r05$r22*7+i5jtwh)sx)in79^i=c562*8m3m6mrfs2t)u^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'shutterlinkProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shutter_link',
+        'USER': 'postgres',
+        'PASSWORD': '99keroti',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
 
