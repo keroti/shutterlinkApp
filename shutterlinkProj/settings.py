0763055d19103757fcdbdcd87f1538b75e102740
keroti <peterkeroti55@gmail.com>
2023-11-13 13:38:05 +0300

    Build shutterlink image

--- shutterlinkProj/settings.py
+++ shutterlinkProj/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'shutterlinkProj.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shutter_link',
-        'USER': 'postgres',
-        'PASSWORD': '99keroti',
-        'HOST': 'localhost',
-        'PORT': '5433'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app',
+        'USER': 'keroti',
+        'PASSWORD': '@99kigan',
+        'HOST': 'shutterlink.postgres.database.azure.com',
+        'PORT': '5432'
     }
 }
 
